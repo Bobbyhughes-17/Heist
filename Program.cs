52515cd42a824d325ecd53ff5343e6670365e679
Bobby Hughes <bobbyhughes6316@gmail.com>
2023-05-30 14:32:04 -0400

    phase uno

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Plan Your Heist!");
+
+        TeamMember teamMember = new TeamMember();
+
+        Console.Write("Enter team member's name: ");
+        teamMember.Name = Console.ReadLine();
+
+        Console.Write("Enter team member's skill level: ");
+        teamMember.SkillLevel = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter team member's courage factor: ");
+        teamMember.CourageFactor = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("\nTeam Member Information:");
+        Console.WriteLine($"Name: {teamMember.Name}");
+        Console.WriteLine($"Skill Level: {teamMember.SkillLevel}");
+        Console.WriteLine($"Courage Factor: {teamMember.CourageFactor}");
+    }
+}
+
+class TeamMember
+{
+    public string Name { get; set; }
+    public int SkillLevel { get; set; }
+    public double CourageFactor { get; set; }
+}
