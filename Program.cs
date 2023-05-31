7cad1c21b3ad325a45a8c429a24d082c2626ca49
Bobby Hughes <bobbyhughes6316@gmail.com>
2023-05-31 11:08:24 -0400

    phase 3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 class Program
 {
@@ -8,23 +9,46 @@ class Program
 
         //store team members
         List<TeamMember> teamMembers = new List<TeamMember>();
-
+        
         while (true)
         {
 
-        Console.Write("Enter team member's name (or leave blank to finish): ");
+        Console.Write("Enter team member's name (or leave blank to finish adding members): ");
         string name = Console.ReadLine();
 
         // if entered name is empty exit the loop
         if (string.IsNullOrWhiteSpace(name))
         break;
+        //check to see if it's only letters 
+        if (Regex.IsMatch(name, "^[a-zA-Z]+$"))
+        {
+
+        }
+        else
+        {
+            Console.WriteLine("Invalid input must be letters only yo");
+            continue;
+        }
 
         Console.Write("Enter team member's skill level: ");
         int skillLevel = int.Parse(Console.ReadLine());
 
-        Console.Write("Enter team member's courage factor (0.0-2.0): ");
-        double courageFactor = double.Parse(Console.ReadLine());
 
+        double courageFactor;
+        while (true)
+        {
+        Console.Write("Enter team member's courage factor (0.0-2.0): ");
+        string input = Console.ReadLine();
+
+        if (double.TryParse(input, out courageFactor))
+        {
+            if (courageFactor >= 0.0 && courageFactor <= 2.0)
+            {
+               break;
+            }
+        }
+        Console.WriteLine("Invalid input");
+        }
         // create a new team member object with info
        TeamMember member = new TeamMember(name, skillLevel, courageFactor);
 
@@ -33,18 +57,27 @@ class Program
        Console.WriteLine();
     }
 
-    Console.WriteLine($"Number of team members: {teamMembers.Count}");
+    int bankDifficultyLevel = 100;
+    int totalSkillLevel = 0;
 
-    Console.WriteLine("Team Members: ");
-
-    // go through each member and print their details
     foreach (var member in teamMembers)
     {
-        Console.WriteLine($"Name: {member.Name}");
-        Console.WriteLine($"Skill Level: {member.SkillLevel}");
-        Console.WriteLine($"Courage Factor: {member.CourageFactor}");
-        Console.WriteLine();
+        totalSkillLevel += member.SkillLevel;
     }
+
+    Console.WriteLine("-- Heist Report --");
+    Console.WriteLine($"Total Team Skill Level: {totalSkillLevel}");
+    Console.WriteLine($"Bank Difficulty Level: {bankDifficultyLevel}");
+
+    if (totalSkillLevel >= bankDifficultyLevel)
+    {
+        Console.WriteLine("Success! Skill level was enough!");
+    }
+    else
+    {
+        Console.WriteLine("Failure! Skill level was not enough!");
+    }
+
 }
 }
 
