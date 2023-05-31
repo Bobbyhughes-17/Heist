96a4869ccc0b86aa8f216b6ffe3efc93908809ac
Bobby Hughes <bobbyhughes6316@gmail.com>
2023-05-31 10:08:01 -0400

    phase 2

--- Program.cs
+++ Program.cs
@@ -6,22 +6,46 @@ class Program
     {
         Console.WriteLine("Plan Your Heist!");
 
-        TeamMember teamMember = new TeamMember();
+        //store team members
+        List<TeamMember> teamMembers = new List<TeamMember>();
 
-        Console.Write("Enter team member's name: ");
-        teamMember.Name = Console.ReadLine();
+        while (true)
+        {
+
+        Console.Write("Enter team member's name (or leave blank to finish): ");
+        string name = Console.ReadLine();
+
+        // if entered name is empty exit the loop
+        if (string.IsNullOrWhiteSpace(name))
+        break;
 
         Console.Write("Enter team member's skill level: ");
-        teamMember.SkillLevel = int.Parse(Console.ReadLine());
+        int skillLevel = int.Parse(Console.ReadLine());
 
-        Console.Write("Enter team member's courage factor: ");
-        teamMember.CourageFactor = double.Parse(Console.ReadLine());
+        Console.Write("Enter team member's courage factor (0.0-2.0): ");
+        double courageFactor = double.Parse(Console.ReadLine());
 
-        Console.WriteLine("\nTeam Member Information:");
-        Console.WriteLine($"Name: {teamMember.Name}");
-        Console.WriteLine($"Skill Level: {teamMember.SkillLevel}");
-        Console.WriteLine($"Courage Factor: {teamMember.CourageFactor}");
+        // create a new team member object with info
+       TeamMember member = new TeamMember(name, skillLevel, courageFactor);
+
+       // add new member to teamMembers list
+       teamMembers.Add(member);
+       Console.WriteLine();
     }
+
+    Console.WriteLine($"Number of team members: {teamMembers.Count}");
+
+    Console.WriteLine("Team Members: ");
+
+    // go through each member and print their details
+    foreach (var member in teamMembers)
+    {
+        Console.WriteLine($"Name: {member.Name}");
+        Console.WriteLine($"Skill Level: {member.SkillLevel}");
+        Console.WriteLine($"Courage Factor: {member.CourageFactor}");
+        Console.WriteLine();
+    }
+}
 }
 
 class TeamMember
@@ -29,4 +53,12 @@ class TeamMember
     public string Name { get; set; }
     public int SkillLevel { get; set; }
     public double CourageFactor { get; set; }
+
+    public TeamMember(string name, int skillLevel, double courageFactor)
+    {
+        Name = name;
+        SkillLevel = skillLevel;
+        CourageFactor = courageFactor;
+    }
 }
+
